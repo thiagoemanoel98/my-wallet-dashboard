3d4f111c1e03877fbcfb275bd55de48ef2dcc0ad
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-01-31 10:58:24 -0300

    ✨ opções do menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -2,10 +2,44 @@ import React from "react";
 
 import * as S from "./styles";
 
+import logoImg from "../../assets/logo.svg";
+
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
 const Aside: React.FC = () => {
   return (
     <S.Container>
-      <h1>Aside</h1>
+      <S.Header>
+        <S.LogoImg src={logoImg} alt="Logo MyWallet" />
+        <S.Title>MyWallet</S.Title>
+      </S.Header>
+
+      <S.MenuContainer>
+        <S.MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </S.MenuItemLink>
+
+        <S.MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </S.MenuItemLink>
+
+        <S.MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </S.MenuItemLink>
+
+        <S.MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </S.MenuItemLink>
+      </S.MenuContainer>
     </S.Container>
   );
 };
