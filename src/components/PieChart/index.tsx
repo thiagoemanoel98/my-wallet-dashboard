837a28e4f0d6300ca5f7a257dab30ecd473584c0
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-03-03 17:59:46 -0300

    ✨ PieChartBox component

---
+++ src/components/PieChart/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { Pie, ResponsiveContainer, PieChart, Cell } from "recharts";
+
+import * as S from "./styles";
+
+interface IPieChartProps {
+  data: {
+    name: string;
+    value: number;
+    percent: number;
+    color: string;
+  }[];
+}
+
+// Componente puro: Não possui states + performace
+// Ao usar parenteses, já sabe que vai ser pure component
+
+const PieChartBox: React.FC<IPieChartProps> = ({ data }) => (
+  <S.Container>
+    <S.SideLeft>
+      <h2>Relação</h2>
+      <S.LegendContainer>
+        {data.map((indicator) => (
+          <S.Legend color={indicator.color} key={indicator.name}>
+            <div>{indicator.percent}</div>
+            <span>{indicator.name}</span>
+          </S.Legend>
+        ))}
+      </S.LegendContainer>
+    </S.SideLeft>
+
+    <S.SideRight>
+      <ResponsiveContainer  >
+        <PieChart>
+          <Pie data={data} dataKey={"percent"}>
+            {data.map((indicator) => (
+              <Cell key={indicator.name} fill={indicator.color} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </S.SideRight>
+  </S.Container>
+);
+
+export default PieChartBox;
