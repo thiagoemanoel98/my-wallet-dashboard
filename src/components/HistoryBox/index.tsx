66f9a3c23c13f4c9bd7e50d9eb81233f7a5b68e2
Thiago Emanoel <thiagoemanoel448@gmail.com>
2023-03-11 11:32:52 -0300

    Fix: Formatação valores

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -9,6 +9,8 @@ import {
   Tooltip,
 } from "recharts";
 
+import formatCurrency from "../../utils/formatCurrency";
+
 import * as S from "./styles";
 
 interface IHistoryBoxProps {
@@ -50,7 +52,7 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
         >
           <CartesianGrid strokeDasharray={"3 3"} stroke="#cecece" />
           <XAxis dataKey={"month"} stroke="#cecece" />
-          <Tooltip />
+          <Tooltip formatter={(value) => formatCurrency(Number(value))} />
 
           <Line
             type={"monotone"}
